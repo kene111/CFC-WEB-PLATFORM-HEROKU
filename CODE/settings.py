0884a97985b031b3a0c70947d8a269df9f72d6bd
Kenechi Ojukwu <kenechiojukwu@gmail.com>
2020-07-23 21:54:37 +0100

    DEBUG Configure

--- CODE/settings.py
+++ CODE/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True #True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True') #True
 
 ALLOWED_HOSTS = ['n-dia.herokuapp.com']
 
