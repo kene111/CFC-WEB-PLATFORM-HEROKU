e400712afe0f505c94e61a78f286cdce81ff305e
Kenechi Ojukwu <kenechiojukwu@gmail.com>
2020-07-23 20:58:10 +0100

    Host issues solved

--- CODE/settings.py
+++ CODE/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True #True
 
-ALLOWED_HOSTS = ['https://n-dia.herokuapp.com/chatbot/ndia/']
+ALLOWED_HOSTS = ['n-dia.herokuapp.com']
 
 
 # Application definition
