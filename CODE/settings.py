52ce30bb20da0543152a5ed8b82b43c6b11408f9
Kenechi Ojukwu <kenechiojukwu@gmail.com>
2020-07-24 10:21:33 +0100

    allowed host

--- CODE/settings.py
+++ CODE/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True') #True
 
-ALLOWED_HOSTS = [] # 'n-dia.herokuapp.com'
+ALLOWED_HOSTS = ['n-dia.herokuapp.com'] # 'n-dia.herokuapp.com'
 
 
 # Application definition
